dc8a81763ed9f17d47b70c4af39e5479e5fd275e
Gjert <gmhomb@stud.ntnu.no>
2020-10-24 20:00:02 +0200

    Added the correct roles

--- site-modules/role/manifests/manager_server.pp
+++
@@ -1,5 +0,0 @@
-class role::manager_server {
-  include ::profile::base_linux
-  include ::profile::dns::client
-  include ::profile::consul::server
-}
--- site-modules/role/manifests/monitoring_server.pp
+++
@@ -1,6 +0,0 @@
-class role::monitoring_server {
-  include ::profile::base_linux
-  include ::profile::dns::client
-  include ::profile::consul::server
-  include ::profile::sensu::backend
-}
