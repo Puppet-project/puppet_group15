736d380b325769e1bd557aa0810ed97ad5a2a5fd
Christian Isnes <cisnes@outlook.com>
2020-10-28 14:09:53 +0100

    Reworked syntax for metricbeat

--- site-modules/profile/manifests/beats/metricbeat.pp
+++ site-modules/profile/manifests/beats/metricbeat.pp
@@ -1,22 +1,24 @@
-class profile::beats::metricbeat {
-  modules => [
-    {
-      'module'     => 'system',
-      'metricsets' => [
-        'cpu',
-        'load',
-        'memory',
-        'process',
-        'uptime',
-        'diskio'
-      ],
-      'processes'  => ['.*'],
+class profile::beats::metricbeat{
+  class{'metricbeat':
+    modules => [
+      {
+        'module'     => 'system',
+        'metricsets' => [
+          'cpu',
+          'load',
+          'memory',
+          'process',
+          'uptime',
+          'diskio'
+        ],
+        'processes'  => ['.*'],
+      },
+    ],
+    outputs => {
+      'elasticsearch' => {
+        'hosts' => ['http://elasticsearch:9200'],
+        'index' => 'metricbeat',
+      },
     },
-  ],
-  outputs => {
-    'elasticsearch' => {
-      'hosts' => ['http://elasticsearch:9200'],
-      'index' => 'metricbeat',
-    },
-  },
-}
+  }
+} 
