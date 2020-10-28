fae0a5b324dec2840f09ad5afaa0f98559440b7d
Gjert <gmhomb@stud.ntnu.no>
2020-10-28 22:34:17 +0100

    Added fix for older java

--- site-modules/profile/manifests/elk/elasticsearch.pp
+++ site-modules/profile/manifests/elk/elasticsearch.pp
@@ -1,11 +1,35 @@
+#  
+# Puppet code for configuring elasticsearch
+#
+
 class profile::elk::elasticsearch {
   include ::java
 
   class { 'elasticsearch':
   restart_on_change => true,
+  jvm_options       => [
+      '-Xms16000m',
+      '-Xmx16000m',
+      '8:-XX:NumberOfGCLogFiles=32',
+      '8:-XX:GCLogFileSize=64m',
+      '8:-XX:+UseGCLogFileRotation',
+      '8:-XX:+PrintTenuringDistribution',
+      '8:-XX:+PrintGCDateStamps',
+      '8:-XX:+PrintGCApplicationStoppedTime',
+      '8:-XX:+UseConcMarkSweepGC',
+      '8:-XX:+UseCMSInitiatingOccupancyOnly',
+      '11:-XX:+UseG1GC',
+      '11:-XX:InitiatingHeapOccupancyPercent=75'
+      ]
   }
-  
-  elasticsearch::instance { 'elk1': }
+
+  #fixes a bug where elastic cant find the conf file.
+  file_line { 'path_conf':
+    path => '/etc/default/elasticsearch',
+    line => 'ES_PATH_CONF=/etc/elasticsearch/elk1',
+}
+
+  elasticsearch::instance { 'elk1':}
 }
 
 
