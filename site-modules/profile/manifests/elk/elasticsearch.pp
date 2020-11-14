3b2d7ff4ac07822202cf188cfb4d100f9d314dba
Gjert <gmhomb@stud.ntnu.no>
2020-11-14 18:59:53 +0100

    added instance back

--- site-modules/profile/manifests/elk/elasticsearch.pp
+++ site-modules/profile/manifests/elk/elasticsearch.pp
@@ -8,33 +8,33 @@ class profile::elk::elasticsearch {
 
   class { 'elasticsearch':
     restart_on_change => true,
-    #config            => {
-    #  'network.host' => '0.0.0.0'
-    #},
-  }
+    config            => {
+      'network.host'  => '0.0.0.0'
+    },
+
   #elasticsearch dont work properly on jdk11. This removes deprecated functions
-#  jvm_options             => [
-#      '8:-XX:NumberOfGCLogFiles=32',
-#      '8:-XX:GCLogFileSize=64m',
-#      '8:-XX:+UseGCLogFileRotation',
-#      '8:-XX:+PrintTenuringDistribution',
-#      '8:-XX:+PrintGCDateStamps',
-#      '8:-XX:+PrintGCApplicationStoppedTime',
-#      '8:-XX:+UseConcMarkSweepGC',
-#      '8:-XX:+UseCMSInitiatingOccupancyOnly',
-#      '8:-XX:CMSInitiatingOccupancyFraction=75',
-#      '11:-XX:+UseG1GC',
-#      '11:-XX:InitiatingHeapOccupancyPercent=75'
-#      ]
-#  }
+  jvm_options         => [
+      '8:-XX:NumberOfGCLogFiles=32',
+      '8:-XX:GCLogFileSize=64m',
+      '8:-XX:+UseGCLogFileRotation',
+      '8:-XX:+PrintTenuringDistribution',
+      '8:-XX:+PrintGCDateStamps',
+      '8:-XX:+PrintGCApplicationStoppedTime',
+      '8:-XX:+UseConcMarkSweepGC',
+      '8:-XX:+UseCMSInitiatingOccupancyOnly',
+      '8:-XX:CMSInitiatingOccupancyFraction=75',
+      '11:-XX:+UseG1GC',
+      '11:-XX:InitiatingHeapOccupancyPercent=75'
+      ]
+  }
 
   #fixes a bug where elastic cant find the conf file.
-#  file_line { 'path_conf':
-#    path => '/etc/default/elasticsearch',
-#    line => 'ES_PATH_CONF=/etc/elasticsearch/elk1',
-#}
+  file_line { 'path_conf':
+    path => '/etc/default/elasticsearch',
+    line => 'ES_PATH_CONF=/etc/elasticsearch/elk1',
+  }
 
-  #elasticsearch::instance { 'elk1':}
+  elasticsearch::instance { 'es-01':}
 }
 
 
