be3ff94f1e9f9b12e1aec8bb910a3b032d54c392
Gjert <gjert@DESKTOP-1M4OBRP.localdomain>
2020-11-18 00:48:38 +0100

    added configuration for kibana

--- site-modules/profile/manifests/elk/kibana.pp
+++ site-modules/profile/manifests/elk/kibana.pp
@@ -5,11 +5,11 @@
 class profile::elk::kibana {
   class { 'kibana':
     config => {
-    'server.port'                 => '8080',
+    'server.port'                 => 8080,
     'server.host'                 => '0.0.0.0',
-    'xpack.security.enabled'      => 'true',
-    'elasticsearch.hosts'         => '[https://elasticsearch:9200]',
-    'xpack.ingestManager.enabled' => 'true',
+    'xpack.security.enabled'      => true,
+    'elasticsearch.hosts'         => ['http://elasticsearch:9200'],
+    'xpack.ingestManager.enabled' => true,
     #'elasticsearch.password'      => '',
     #'elasticsearch.username'      => '',
     },
