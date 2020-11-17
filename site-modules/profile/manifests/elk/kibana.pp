e61e0c5c9b78b893e071605f4ad880beb4231701
Gjert <gjert@DESKTOP-1M4OBRP.localdomain>
2020-11-17 21:39:19 +0100

    Added some ocnf options to kibana

--- site-modules/profile/manifests/elk/kibana.pp
+++ site-modules/profile/manifests/elk/kibana.pp
@@ -5,9 +5,13 @@
 class profile::elk::kibana {
   class { 'kibana':
     config => {
-    'server.port'            => '8080',
-    'server.host'            => '0.0.0.0',
-    'xpack.security.enabled' => 'true',
+    'server.port'                 => '8080',
+    'server.host'                 => '0.0.0.0',
+    'xpack.security.enabled'      => 'true',
+    'elasticsearch.hosts'         => '[https://elasticsearch:9200]',
+    'xpack.ingestManager.enabled' => 'true',
+    #'elasticsearch.password'      => '',
+    #'elasticsearch.username'      => '',
     },
 
   }
