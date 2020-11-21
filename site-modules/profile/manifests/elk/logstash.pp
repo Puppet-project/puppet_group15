fa45392f28bf6778ed1a83e71a468657216989e7
Gjert <gmhomb@stud.ntnu.no>
2020-11-21 16:04:14 +0100

    added java fix

--- site-modules/profile/manifests/elk/logstash.pp
+++ site-modules/profile/manifests/elk/logstash.pp
@@ -5,6 +5,13 @@
 class profile::elk::logstash {
 
   class { 'logstash':
+    #fixes a problem where logstart wont start
+    jvm_options => [
+      '-DUseParNewGC=disable',
+      '-DUseConcmarksweepgc=disable',
+      '-XX:+UseG1GC',
+      'SuccessExitStatus=143',
+    ],
   }
   logstash::configfile {'my_ls_config':
     content => template('/etc/puppetlabs/code/environments/dev/data/logstash.conf'),
