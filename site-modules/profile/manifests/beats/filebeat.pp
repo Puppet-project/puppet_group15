8fa66b0db5eaebf75f92086d644001b5dd678875
Gjert <gmhomb@stud.ntnu.no>
2020-11-21 15:20:14 +0100

    Added filebeat

---
+++ site-modules/profile/manifests/beats/filebeat.pp
@@ -0,0 +1,22 @@
+#configuration for setting up filebeat
+
+class profile::beats::filebeat{
+  class { 'filebeat':
+  outputs => {
+    'logstash' => {
+    'hosts'       => [
+      'logstash:5044',
+    ],
+    'loadbalance' => true,
+    },
+  },
+  }
+
+  filebeat::input { 'syslogs':
+  paths    => [
+    '/var/log/auth.log',
+    '/var/log/syslog',
+  ],
+  doc_type => 'syslog-beat',
+  }
+}
