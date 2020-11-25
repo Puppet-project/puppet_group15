a0047b927c63ed6aea3fdb92799d7b50dbcecd55
Christian Isnes <cisnes@outlook.com>
2020-11-25 17:31:45 +0100

    Adding DNS hosts back

--- site-modules/profile/manifests/beats/winlogbeat.pp
+++ site-modules/profile/manifests/beats/winlogbeat.pp
@@ -6,7 +6,7 @@ class profile::beats::winlogbeat{
   class {'winlogbeat':
     outputs => {
       'logstash'  => {
-        'hosts' => [ 'http://10.212.142.103:5044' ],
+        'hosts' => [ 'http://logstash:5044' ],
         'index' => 'winlogbeat',
       },
     conf_template => 'C:\ProgramData\PuppetLabs\code\environments\production\data\winlogbeat.yml'
