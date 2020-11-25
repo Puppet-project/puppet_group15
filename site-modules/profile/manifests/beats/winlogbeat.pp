cfbd3bb045ffdae061f8841b107b765cc08eda45
Gjert <gmhomb@stud.ntnu.no>
2020-11-25 12:48:30 +0100

    changed conf path

--- site-modules/profile/manifests/beats/winlogbeat.pp
+++ site-modules/profile/manifests/beats/winlogbeat.pp
@@ -5,11 +5,11 @@
 class profile::beats::winlogbeat{
   class {'winlogbeat':
     outputs => {
-      'logstash' => {
+      'logstash'  => {
         'hosts' => [ 'http://logstash:5044' ],
         'index' => 'winlogbeat',
       },
-    conf_template => 'C:\ProgramData\PuppetLabs\code\environments\dev\data\winlogbeat.yml'
+    conf_template => 'C:\ProgramData\PuppetLabs\code\environments\production\data\winlogbeat.yml'
     },
   }
 }
